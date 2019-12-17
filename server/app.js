cbe77ad74c9e44db1a0bf34c79acf8d1ed718265
Josiah Swab <josiahjswab@gmail.com>
2019-12-17 14:47:38 -0800

    Adds a completed project.

--- server/app.js
+++ server/app.js
@@ -1,20 +1,47 @@
 const express = require('express');
 const fs = require('fs');
 const app = express();
+const csv = require('csvtojson');
 
-app.use((req, res, next) => {
-// write your logging code here
+app.use(logAllTheThings);
 
-});
+var log = [];
 
+/**
+ *  Custom logger middleware.
+ *  Previous log is erased so as not to append to old array data.
+ *  Date().toISOString was a requirment to pass the test.
+ *  req.protocol.toUpperCase was used to pass the test.
+ */
+function logAllTheThings(req, res, next) {
+  
+  log = []; 
+  log.push(req.header('user-agent').replace(/,/g, ' '));
+  log.push(new Date().toISOString()); 
+  log.push(req.method);
+  log.push(req.originalUrl);
+  log.push(`${req.protocol.toUpperCase()}/${req.httpVersion}`); 
+  log.push(res.statusCode);
+
+  fs.appendFile(
+    'log.csv', `\n ${log.toString()}`,
+    (err) => {if(err) throw err, console.log(err); else console.log("Traffic added to log.")});
+  next();
+};
+
+// This console.log(log.toString()) is necessary to pass tests.
 app.get('/', (req, res) => {
-// write your code to respond "ok" here
-
+  res.send("ok");
+  console.log(log.toString()) 
 });
 
+// Endpoint that views log.csv.
 app.get('/logs', (req, res) => {
-// write your code to return a json object containing the log data here
-
+  csv()
+    .fromFile('./log.csv')
+    .then((logData) => {
+      res.status(200).send(logData);
+    });
 });
 
 module.exports = app;
